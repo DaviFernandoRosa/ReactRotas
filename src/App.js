0fc75889c401ce9e72e6c23b662abbb841d71fc7
Davi Fernando Rosa <79165566+DaviFernandoRosa@users.noreply.github.com>
2021-09-12 18:18:53 -0300

    Update App.js
    Remoção de espaços...

--- src/App.js
+++ src/App.js
@@ -8,83 +8,8 @@ function App() {
     
     <div>
        <Rotas />
-    </div>
-  
-   
+    </div>  
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-import React from 'react';
-import Home from './components/Home/Home';
-import Header from './components/Header/Header';
-import Produtos from './components/Produtos/produtos';
-import Aviso from './components/Aviso/Avisos';
-import Footer from './components/Footer/Footer';
-import {
-
-  BrowserRouter as Router,
-  Route
-
-} from 'react-router-dom'
-
-import './App.css';
-
-
-function App() {
-  return (
-    <Router>
-          <Header />
-         <Route path='/' exact component={Home} />
-         <Route  path='/produtos'  component={Produtos} />
-         <Route  path='/aviso' component={Aviso} />
-
-
-          <Footer />
-
-          
-      
-    </Router>
-      
-   
-
-
-
-    
-  );
-}
-
-export default App; */
